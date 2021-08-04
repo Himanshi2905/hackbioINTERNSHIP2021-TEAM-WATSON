642ef41534e7c55def01348a807923cec071f1bb
parthkhurana07 <88340886+parthkhurana07@users.noreply.github.com>
2021-08-04 23:27:48 +0530

    Add files via upload

---
+++ Parth_R_intro.R
@@ -0,0 +1,8 @@
+name= c('Name- Parth Khurana')
+email= c('Email- khuranaparth07@gmail.com')
+slack= c('Slack Username- @Parth')
+twitter= c('Twitter username- @pk707')
+biostack= c('Biostack- Genomics & Drug Development')
+print(c(name, email, slack, twitter, biostack))
+stringdist("@parth","@pk707",method="h")
+
