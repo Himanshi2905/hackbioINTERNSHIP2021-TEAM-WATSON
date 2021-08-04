1f3fab29e6b333a8606e3bed19118683f865a61d
Himanshi2905 <kunduhimanshi@gmail.com>
2021-08-04 07:59:30 -0700

    first commit

---
+++ scripthimanshi.py
@@ -0,0 +1,7 @@
+name = 'Himanshi Kundu'
+email = 'kunduhimanshi@gmail.com'
+slack = '@Himanshi'
+biostack = 'Drug development'
+twitter = '@itsanshi'
+hammingdistance = '3'
+print('{}, {}, {}, {},{},{}'.format(name,email,slack,biostack,twitter,hammingdistance))
