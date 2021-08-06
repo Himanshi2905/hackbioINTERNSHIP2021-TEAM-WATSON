5feaffdffd013b84858da26bf1fb26d61375620a
parthkhurana07 <88340886+parthkhurana07@users.noreply.github.com>
2021-08-06 13:26:57 +0530

    Delete Parth_R_intro.R

--- Parth_R_intro.R
+++
@@ -1,9 +0,0 @@
-name= c('Name- Parth Khurana')
-email= c('Email- khuranaparth07@gmail.com')
-slack= c('Slack Username- @Parth')
-twitter= c('Twitter username- @pk707')
-biostack= c('Biostack- Genomics & Drug Development')
-hamming_distance= stringdist("@parth","@pk707",method="h")
-info_list= c(name,email,slack,twitter,biostack,hamming_distance)
-cat(paste(shQuote(info_list, type= "cmd"), collapse=", "))
-
