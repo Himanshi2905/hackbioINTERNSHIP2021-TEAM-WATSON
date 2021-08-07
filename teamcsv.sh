d5d33daa686adb2e2a05a0cab68530883738bd5b
Himanshi2905 <88250922+Himanshi2905@users.noreply.github.com>
2021-08-07 20:50:10 +0530

    Add files via upload

---
+++ teamcsv.sh
@@ -0,0 +1,11 @@
+#Team Watson Bash Script
+
+rm team_watson.csv
+Rscript  scriptparth.R >> 'team_watson.csv'
+python3  scripthimanshi.py >> 'team_watson.csv'
+python3  scriptsomoshri.py >> 'team_watson.csv'
+Rscript  scriptparth.R   >> 'team_watson.csv'
+java     scripteao.java >> 'team_watson.csv'
+g++      scriptbhoopesh.cpp  && ./a.out >> 'team_watson.csv'
+perl scriptmahalakshmi.pl >> 'team_watson.csv'
+php  scriptsaran.php >> 'team_watson.csv'
