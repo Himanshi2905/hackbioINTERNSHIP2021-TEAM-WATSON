09b8a7f6cdf9c32eff23c7080062c3b8b4035528
GEMADURA <88286612+GEMADURA@users.noreply.github.com>
2021-08-07 22:15:58 +0530

    Add files via upload

---
+++ team_watto.sh
@@ -0,0 +1,19 @@
+#Team Watson Bash Script
+
+rm team watson.csv
+
+Rscript scriptparth.R >> 'team_watson.csv'
+
+python3 scripthimanshi.py >> 'team_watson.csv'
+
+python3 scriptsomoshri.py >> 'team_watson.csv'
+
+Rscript scriptparth.R >> 'team watson.csv'
+
+java scripteao.java >> 'team_watson.csv'
+
+g++ scriptbhoopesh.cpp && ./a.out >> 'team_watson.csv'
+
+perl scriptmahalakshmi.pl >> 'team watson.csv'
+
+php scriptsaran.php >> 'team_watson.csv'
